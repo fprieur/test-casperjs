18165300ce6c5e6d6fdc033ca5c6d636caf0b56b
Fred Prieur <fprieur@gmail.com>
2014-04-01 16:02:37 -0400

    allo

--- tests/ghost/app_test.js
+++ tests/ghost/app_test.js
@@ -1,8 +1,11 @@
-casper
-.start(casper.cli.get('baseUrl'))
-.then(function () {
-    this.test.assertTitle("Portail données ouvertes", "le titre est bien celui que l'on attendait");    
-})
+casper.test.begin("Testez des afffaires", 1, function suite(test){
+casper.start("http://www.donnees.ville.montreal.qc.ca", function() {
+    .then(function () {
+        this.test.assertTitle("Portail données ouvertes", "le titre est bien celui que l'on attendait");    
+    })
+});
+
+});
 .run(function () {
 	this.test.done();
 });
